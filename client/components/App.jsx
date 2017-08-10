ca93f83b462d52762f134bef75c651e351394551
Chris Chou <chhschou@gmail.com>
2017-08-11 11:59:06 +1200

    add search bar and basic search

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,12 +3,14 @@ import {HashRouter as Router, Route} from 'react-router-dom'
 import BookList from './BookList'
 
 
+import Search from './Search'
+
 const App = () => (
   <Router>
     <div className='app-container'>
       <h1>Pegasus Booklist</h1>
+      <Search />
       <Route exact path='/' component={BookList}/>
-
     </div>
   </Router>
 )
--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import { doSearch } from '../actions/searchResult'
 
-export default function () {
+function getKeywords() {
+  let keyword = document.getElementById('searchkeyword').value
+  return keyword.split(' ')
+}
+
+
+function Search({books, dispath }) {
   return (
     <div className='search'>
-
+      <input id='searchkeyword' type='text' name='keyword' />
+      <button onClick={() => doSearch(books, getKeywords(), dispatch) } >Search</button>
     </div>
   )
 }
+
+function mapStateToProps (state) {
+  return {
+    books: state.books
+  }
+}
+
+export default connect(mapStateToProps)(Search)
