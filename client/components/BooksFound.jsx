d14316618b95e54b4a9231bfe0a0310bef1e2af7
mischa-saunders <mischa.saunders@gmail.com>
2017-08-11 15:27:48 +1200

    Search Function working

--- client/components/BooksFound.jsx
+++ client/components/BooksFound.jsx
@@ -2,17 +2,23 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 
-function BooksFound ({books}) {
-console.log(books);
+function BooksFound({books}) {
+  console.log(books);
   return (
     <div className='book'>
-      <ul>
-      {
-      books.map((book, k) =>
-      {
-        return (<li>{book.title}</li>)
-      })}
+
+      {books.map((book, k) => {
+        return (
+          <div className='Result'>
+            <h2>Search Result:</h2>
+            <ul>
+              <li>{book.title}</li>
+              <li>{book.first_name}{book.last_name}</li>
+              <li>{book.genre}</li>
             </ul>
+          </div>
+        )
+      })}
     </div>
   )
 }
