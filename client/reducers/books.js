d5f15fc15d92a3498822fd649f4a6c474bba1028
Chris Chou <chhschou@gmail.com>
2017-08-11 14:04:59 +1200

    update with 3 more books

--- client/reducers/books.js
+++ client/reducers/books.js
@@ -1,16 +1,33 @@
 import {
   RECEIVE_BOOKS
 } from './index'
-const initialState = [
+const initialState = [{
+    id: 1,
+    title: "Solaris",
+    author_id: 1,
+    genre_id: 1,
+    first_name: "Stanislaw",
+    last_name: "Lem",
+    genre: "Science Fiction"
+  },
   {
-      id: 2,
-      title: "Every Day is for the Thief",
-      author_id: 2,
-      genre_id: 2,
-      first_name: "Teju",
-      last_name: "Cole",
-      genre: "Novel"
-    }
+    id: 2,
+    title: "Every Day is for the Thief",
+    author_id: 2,
+    genre_id: 2,
+    first_name: "Teju",
+    last_name: "Cole",
+    genre: "Novel"
+  },
+  {
+    id: 3,
+    title: "Fifty Shades of Grey",
+    author_id: 3,
+    genre_id: 3,
+    first_name: "E L",
+    last_name: "James",
+    genre: "Provocative Romance"
+  }
 ]
 
 export default function (state = initialState, action) {
